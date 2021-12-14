020bee732bcb6174aa7a695c413ee53354ca50d4
Lari Oncescu <larioncescu@yahoo.com>
2021-12-14 12:30:00 +0000

    reset score on start

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//Selecting elements
+const diceImg = document.querySelector('.dice');
+const firstScoreEl = document.querySelector('#score--0');
+const secondScoreEl = document.getElementById('score--1');
+
+//Start reset
+firstScoreEl.textContent = 0;
+secondScoreEl.textContent = 0;
+diceImg.classList.add('hide');
